bbe0f3d839a9909ce45d26d0a9ac3de99bf8d5f3
kasper.storgaard <kasper.storgaard@gmail.com>
2017-03-23 20:20:04 +0100

    test(server): remove listen on port 3000 (let supertest handle spinning/mocking express)

--- server/server.spec.js
+++ server/server.spec.js
@@ -4,18 +4,14 @@ import subject from './server';
 
 test('gets a response', async t => {
     const app = subject();
-    const server = app.listen(3000);
     const response = await request(app).get('/');
 
     t.is(response.status, 200); 
-    server.close();
 });
 
 test('gets `Hello World!` response', async t => {
     const app = subject();
-    const server = app.listen(3001);
     const response = await request(app).get('/');
 
     t.is(response.text, "Hello World!"); 
-    server.close();
 });

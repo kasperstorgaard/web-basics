af7433cba7f244b40fa78cbf56ccb77ffa66877e
kasper.storgaard <kasper.storgaard@gmail.com>
2017-04-14 11:45:31 +0200

    trying out rollup can custom elements

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+const path = require('path');
 const express = require('express');
 
-function setup() {
+/**
+ * Starts an express instance that serves the index.html file.
+ */
+function start() {
     const app = express();
 
-    app.get('/', (request, response) => response.send('Hello World!'));
+    const distPath = path.join(__dirname, '../dist');
+    const indexPath = path.join(distPath, 'index.html');
+
+    app.use('/dist', express.static(distPath));
+    app.get('/', (request, response) => response.sendFile(indexPath));
 
     return app;
 }
 
-module.exports = setup;
+module.exports = start;
